341e9c3143220d45f208c7906fc9bf877ea19fad
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2023-12-03 00:05:53 +0900

    Add comments and so on.
    Signed-off-by: Yuya Asano <64895419+sukeya@users.noreply.github.com>

--- double.cu
+++ double.cu
@@ -5,12 +5,15 @@
 #include "double.h"
 
 thrust::device_event Double(thrust::host_vector<float>& floats, thrust::host_vector<int>& ints) {
+  // デバイス側の配列を用意
   auto device_floats = thrust::device_vector<float>();
   auto device_ints   = thrust::device_vector<int>();
 
+  // メモリの確保と初期化
   device_floats.resize(floats.size());
   device_ints.resize(ints.size());
 
+  // ホストからデバイスへの非同期コピー
   auto copy_ints_event = thrust::async::copy(
       thrust::host,
       thrust::device,
@@ -18,13 +21,15 @@ thrust::device_event Double(thrust::host_vector<float>& floats, thrust::host_vec
       ints.end(),
       device_ints.begin()
   );
-  auto copy_doubles_event = thrust::async::copy(
+  auto copy_floats_event = thrust::async::copy(
       thrust::host,
       thrust::device,
       floats.begin(),
       floats.end(),
       device_floats.begin()
   );
+
+  // デバイス側での計算
   auto double_ints_event = thrust::async::transform(
       thrust::device.after(copy_ints_event),
       device_ints.begin(),
@@ -32,25 +37,28 @@ thrust::device_event Double(thrust::host_vector<float>& floats, thrust::host_vec
       device_ints.begin(),
       [] __device__(int i) { return i * i; }
   );
-  auto double_doubles_event = thrust::async::transform(
-      thrust::device.after(copy_doubles_event),
+  auto double_floats_event = thrust::async::transform(
+      thrust::device.after(copy_floats_event),
       device_floats.begin(),
       device_floats.end(),
       device_floats.begin(),
       [] __device__(float d) { return d * d; }
   );
+
+  // デバイスからホストへの非同期コピー
   auto copy_back_ints_event = thrust::async::copy(
       thrust::device.after(double_ints_event),
       device_ints.begin(),
       device_ints.end(),
       ints.begin()
   );
-  auto copy_back_doubles_event = thrust::async::copy(
-      thrust::device.after(double_doubles_event),
+  auto copy_back_floats_event = thrust::async::copy(
+      thrust::device.after(double_floats_event),
       device_floats.begin(),
       device_floats.end(),
       floats.begin()
   );
-
-  return thrust::when_all(copy_back_ints_event, copy_back_doubles_event);
+  
+  // 非同期実行の完了をまとめる
+  return thrust::when_all(copy_back_ints_event, copy_back_floats_event);
 }
--- double.h
+++ double.h
@@ -1,4 +1,4 @@
-#include <thrust/future.h>
+#include <thrust/event.h>
 #include <thrust/host_vector.h>
 
 thrust::device_event Double(thrust::host_vector<float>& doubles, thrust::host_vector<int>& ints);
